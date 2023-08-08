01e5b1b68750dd618ec399522fcb5207be11dae8
Proboskis <nikolasurla22@gmail.com>
2023-08-08 14:58:18 +0200

    7.2 Finished the settings page, added the use-origin hook and the api-alert component.

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -15,6 +15,11 @@ export function MainNav ({
     const routes = [
         {
             href: `/${params.storeId}/settings`,
+            label: 'Overview',
+            active: pathName === `/${params.storeId}`
+        },
+        {
+            href: `/${params.storeId}/settings`,
             label: 'Settings',
             active: pathName === `/${params.storeId}/settings`
         }
