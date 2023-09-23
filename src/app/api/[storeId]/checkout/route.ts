9f65e87e1a93720edd5a190b840e2c0985accc24
Proboskis <nikolasurla22@gmail.com>
2023-09-23 22:02:07 +0200

    16. Added payment with stripe integration.

---
+++ src/app/api/[storeId]/checkout/route.ts
@@ -0,0 +1,84 @@
+import {Stripe} from "stripe";
+import {NextResponse} from "next/server";
+
+import {stripe} from "@/lib/stripe";
+import prismaDatabase from "@/lib/prismadb";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization"
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, {headers: corsHeaders});
+}
+
+export async function POST(
+  request: Request,
+  {params}: { params: { StoreId: string } }
+) {
+  const {productIds} = await request.json();
+
+  if (!productIds || productIds.length === 0) {
+    return new NextResponse("Product ids are required", {status: 400});
+  }
+
+  const products = await prismaDatabase.product.findMany({
+    where: {
+      id: {
+        in: productIds
+      }
+    }
+  });
+
+  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+  products.forEach(product => {
+    line_items.push({
+      quantity: 1,
+      price_data: {
+        currency: 'USD',
+        product_data: {
+          name: product.name
+        },
+        unit_amount: product.price.toNumber() * 100
+      }
+    });
+  });
+
+  // metadata in the session bellow is used to find the order and change the isPaid to true, to realize the payment
+  const order = await prismaDatabase.order.create({
+    data: {
+      storeId: params.storeId,
+      isPaid: false,
+      orderItems: {
+        create: productIds.map((productId: string) => ({
+          product: {
+            connect: {
+              id: productId
+            }
+          }
+        }))
+      }
+    }
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: "payment",
+    billing_address_collection: "required",
+    phone_number_collection: {
+      enabled: true
+    },
+    success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
+    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
+    metadata: {
+      orderId: order.id
+    }
+  });
+
+  return NextResponse.json({url: session.url}, {
+    headers: corsHeaders
+  });
+}
---
+++ src/app/api/webhooks/route.ts
@@ -0,0 +1,71 @@
+import Stripe from "stripe";
+import {headers} from "next/headers";
+import {NextResponse} from "next/server";
+
+import {stripe} from "@/lib/stripe";
+import prismaDatabase from "@/lib/prismadb";
+
+export async function POST(request: Request) {
+
+  // we are not requesting request.json() because this is a webhook, and this is a special case
+  const body = await request.text();
+  const signature = headers().get("Stripe-Signature") as string;
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error: NextResponse) {
+    return new NextResponse(`Webhook error: ${error.message}`, {status: 400});
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+  const address = session?.customer_details?.address;
+
+  // these are all the fields regarding the address on the stripe checkout page
+  const addressComponents = [
+    address?.line1,
+    address?.line2,
+    address?.city,
+    address?.state,
+    address?.postal_code,
+    address?.country
+  ];
+
+  const addressString = addressComponents.filter(component => component !== null).join(", ");
+
+  if (event.type === "checkout.session.completed") {
+    const order = await prismaDatabase.order.update({
+      where: {
+        id: session?.metadata?.orderId
+      },
+      data: {
+        isPaid: true,
+        address: addressString,
+        phone: session?.customer_details?.phone || ""
+      },
+      include: {
+        orderItems: true
+      }
+    });
+
+    const productIds = orderItems.map(orderItem => orderItem.productId);
+
+    await prismaDatabase.product.updateMany({
+      where: {
+        id: {
+          in: [...productIds]
+        }
+      },
+      data: {
+        isArchived: true
+      }
+    });
+  }
+
+  return new NextResponse(null, {status: 200});
+}
